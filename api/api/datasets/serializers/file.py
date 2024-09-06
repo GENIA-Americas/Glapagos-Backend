433d1bc440ca65fcb54eefe2a09a895040e66a5c
yasmanitmf <yasmani@themadfox.com>
2024-09-06 15:15:06 -0400

    ADD: File MIME type validation

--- api/api/datasets/serializers/file.py
+++ api/api/datasets/serializers/file.py
@@ -1,3 +1,4 @@
+import magic
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import FileExtensionValidator
 from rest_framework import serializers
@@ -5,18 +6,26 @@ from api.datasets.enums import FileType
 
 
 class FileUploadSerializer(serializers.Serializer):
-    file = serializers.FileField(validators=[
-        FileExtensionValidator(allowed_extensions=['csv', 'json', 'txt'],
-                               message=_('Only CSV, TXT, and JSON files are allowed.'))
-    ])
+    file = serializers.FileField()
     public = serializers.BooleanField()
 
     def validate(self, attrs):
         attrs = super().validate(attrs)
+        file = attrs['file']
         valid_extensions = ['csv', 'txt', 'json']
-        extension = attrs['file'].name.split('.')[-1]
+        valid_mime_types = ['text/csv', 'application/json', 'text/plain']
+
+        extension = attrs['file'].name.lower().split('.')[-1]
         if extension not in valid_extensions:
             raise serializers.ValidationError({"detail": _('Only CSV, TXT, and JSON files are allowed.')})
+
+        mime = magic.Magic(mime=True)
+        mime_type = mime.from_buffer(file.read(1024))
+        file.seek(0)
+
+        if mime_type not in valid_mime_types:
+            raise serializers.ValidationError({"detail": _('The filetype does not match with the extension')})
+
         attrs['extension'] = FileType(extension)
 
         return attrs
