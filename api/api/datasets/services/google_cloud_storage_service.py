6add76dc0695d8257492e45586e40b0029139528
yasmanitmf <yasmani@themadfox.com>
2024-12-16 17:32:11 -0500

    fix: add gmail

--- api/api/datasets/services/google_cloud_storage_service.py
+++ api/api/datasets/services/google_cloud_storage_service.py
@@ -28,13 +28,15 @@ class GCSService:
         """Create a folder in Google Cloud Storage."""
         try:
             storage_client = storage.Client()
-            bucket = storage_client.bucket(bucket_name)
-
+            bucket = storage_client.get_bucket(bucket_name)
+            print(bucket)
             blobs = list(storage_client.list_blobs(bucket, prefix=f"{folder_name}/", max_results=1))
+            print(blobs)
             if blobs:
                 return
 
             blob = bucket.blob(f"{folder_name}/")
+            print(blob)
             blob.upload_from_string("")
         except Exception as exp:
             raise CloudStorageOperationException(error=str(exp))
