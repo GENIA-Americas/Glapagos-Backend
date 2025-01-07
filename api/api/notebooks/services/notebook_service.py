d6f6782990d031f6a777768070ebf38a255448d6
yasmanitmf <yasmani@themadfox.com>
2025-01-07 15:37:33 -0500

    feat(notebooks): New notebook instances

--- api/api/notebooks/services/notebook_service.py
+++ api/api/notebooks/services/notebook_service.py
@@ -2,8 +2,12 @@ from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from google.api_core.retry import Retry
 from google.api_core.exceptions import NotFound
-from google.cloud import notebooks_v1
-from google.cloud.notebooks_v1 import Instance, StartInstanceRequest, StopInstanceRequest
+from google.cloud import notebooks_v2
+from google.cloud.notebooks_v2 import (
+    Instance, GceSetup, AcceleratorConfig,
+    BootDisk, DataDisk, ServiceAccount,
+    CreateInstanceRequest, StartInstanceRequest, StopInstanceRequest
+)
 
 from api.users.models import User
 from api.notebooks.exceptions import (
@@ -16,17 +20,13 @@ class VertexInstanceService:
     PROJECT_ID = settings.BQ_PROJECT_ID
     MACHINE_TYPE = "n1-standard-2"
     LOCATION = "us-central1-a"
-    VM_IMAGE_PROJECT = "ml-images"
-    VM_IMAGE_NAME = "c0-deeplearning-common-cpu-v20230925-debian-10"
     IDLE_SHUTDOWN_TIMEOUT = "600"
-    # accelerator_config = {"type": notebooks_v1.Instance.AcceleratorType.NVIDIA_TESLA_T4, "core_count": 1},
-    accelerator_config = {}
     OPERATION_DEADLINE_IN_SECONDS = 300
     instances_base_path = f"projects/{PROJECT_ID}/locations/{LOCATION}/instances/"
 
     @classmethod
     def get_instance(cls, instance_id: str) -> Instance:
-        client = notebooks_v1.NotebookServiceClient()
+        client = notebooks_v2.NotebookServiceClient()
         instance_path = f"{cls.instances_base_path}{instance_id}"
         notebook_instance = client.get_instance(name=instance_path)
         return notebook_instance
@@ -35,33 +35,41 @@ class VertexInstanceService:
     def create_instance(cls, instance_id: str, user: User) -> str:
         if not user.gmail:
             raise InvalidGoogleAccountException()
-        client = notebooks_v1.NotebookServiceClient()
+        client = notebooks_v2.NotebookServiceClient()
         parent = f"projects/{cls.PROJECT_ID}/locations/{cls.LOCATION}"
 
-        instance = Instance(
-            machine_type=cls.MACHINE_TYPE,
-            accelerator_config=cls.accelerator_config,
-            vm_image={
-                "project": cls.VM_IMAGE_PROJECT,
-                "image_name": cls.VM_IMAGE_NAME
-            },
-            boot_disk_size_gb = 50,
-            data_disk_size_gb = 50,
-            metadata={
+        gce_setup = GceSetup(
+            machine_type = cls.MACHINE_TYPE,
+            # TODO: Graphics Accelerator
+            # accelerator_configs = [AcceleratorConfig(
+            #     type = AcceleratorConfig.AcceleratorType.NVIDIA_TESLA_T4,
+            #     core_count = 1
+            # )],
+            service_accounts = [ServiceAccount(email = user.service_account.email)],
+            boot_disk = BootDisk(disk_size_gb=150),
+            data_disks = [DataDisk(disk_size_gb=50)],
+            metadata = {
                 "proxy-mode": "service_account",
                 "idle-shutdown": "true",
                 "idle-shutdown-timeout": cls.IDLE_SHUTDOWN_TIMEOUT,
                 "gcs-data-bucket": f"{settings.GCS_NOTEBOOK_BUCKET}/{user.id}"
-            },
-            instance_owners=[user.gmail],
-            service_account=user.service_account.email
+            }
+
         )
 
-        operation = client.create_instance(
+        instance = Instance(
+            name = instance_id,
+            instance_owners=[user.gmail],
+            gce_setup = gce_setup
+        )
+
+        request = CreateInstanceRequest(
             parent=parent,
             instance_id=instance_id,
             instance=instance,
         )
+
+        operation = client.create_instance(request=request)
         operation.result()
 
         retry_policy = Retry(predicate=lambda e: isinstance(e, ValueError), deadline=300)
@@ -88,9 +96,9 @@ class VertexInstanceService:
     def start_instance(cls, instance_id: str) -> str:
         try:
             notebook_instance = cls.get_instance(instance_id)
-            if notebook_instance.state == notebooks_v1.Instance.State.STOPPED:
+            if notebook_instance.state == notebooks_v2.State.STOPPED:
                 request = StartInstanceRequest(name=notebook_instance.name)
-                client = notebooks_v1.NotebookServiceClient()
+                client = notebooks_v2.NotebookServiceClient()
                 operation = client.start_instance(request=request)
                 operation.result()
 
@@ -104,9 +112,9 @@ class VertexInstanceService:
     def stop_instance(cls, instance_id: str) -> None:
         try:
             notebook_instance = cls.get_instance(instance_id)
-            if notebook_instance.state == notebooks_v1.Instance.State.ACTIVE:
-                request = notebooks_v1.StopInstanceRequest(name=notebook_instance.name)
-                client = notebooks_v1.NotebookServiceClient()
+            if notebook_instance.state == notebooks_v2.State.ACTIVE:
+                request = StopInstanceRequest(name=notebook_instance.name)
+                client = notebooks_v2.NotebookServiceClient()
                 operation = client.stop_instance(request=request)
                 operation.result()
 
@@ -116,10 +124,10 @@ class VertexInstanceService:
     @classmethod
     def destroy_instance(cls, instance_id: str) -> bool:
         try:
-            client = notebooks_v1.NotebookServiceClient()
+            client = notebooks_v2.NotebookServiceClient()
             notebook_instance = cls.get_instance(instance_id)
 
-            if notebook_instance.state == notebooks_v1.Instance.State.ACTIVE:
+            if notebook_instance.state == notebooks_v2.State.ACTIVE:
                 cls.stop_instance(instance_id)
 
             operation = client.delete_instance(name=notebook_instance.name)
